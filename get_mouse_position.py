89e8c1d31f579a57ff966f2a414ab5744c8c116c
rodrigo fernandes <digonalha@gmail.com>
2019-02-07 22:51:46 -0200

    Update get_mouse_position.py

--- get_mouse_position.py
+++ get_mouse_position.py
@@ -6,6 +6,6 @@ mouse = Controller()
 
 def get_mouse_position():
     time.sleep(1)
-    print('posicao atual do mouse: {0}'.format(mouse.position))
+    print('mouse position: {0}'.format(mouse.position))
 
 get_mouse_position()
